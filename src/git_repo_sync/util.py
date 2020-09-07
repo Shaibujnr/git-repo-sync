a808d9cc262e84f1b93fce72414089de943beb01
Shaibu Shaibu <s.shaibu.jnr@gmail.com>
2020-09-07 15:54:36 +0100

    Update config file and util clone repository method

--- src/git_repo_sync/util.py
+++ src/git_repo_sync/util.py
@@ -3,8 +3,8 @@ from git import Repo
 from pathlib import Path
 from git_repo_sync.config import (
     GIT_STATE_PATH,
-    GIT_SOURCE_REPO_USERNAME,
-    GIT_SOURCE_REPO_PASSWORD,
+    GIT_SOURCE_USERNAME,
+    GIT_SOURCE_TOKEN,
 )
 
 repo_folder_path: Path = Path(GIT_STATE_PATH)
@@ -21,13 +21,10 @@ def does_repository_exist_locally(repository_name):
 
 def clone_repository(git_url: str) -> Repo:
     repo_url = git_url
-    url_data = git_url.split("@")
-    if len(url_data) == 1:
+    if "@" not in repo_url:
         # no authentication details
         protocol, url = git_url.split("//")
-        repo_url = (
-            f"{protocol}//{GIT_SOURCE_REPO_USERNAME}:{GIT_SOURCE_REPO_PASSWORD}@{url}"
-        )
+        repo_url = f"{protocol}//{GIT_SOURCE_USERNAME}:{GIT_SOURCE_TOKEN}@{url}"
     repo_name = repo_url.split("/")[-1].split(".")[0]
     target_path = repo_folder_path / repo_name
     return Repo.clone_from(url=repo_url, to_path=target_path)
--- tests/test_config.py
+++ tests/test_config.py
@@ -2,6 +2,6 @@ import git_repo_sync.config as config
 
 
 def test_config():
-    assert config.GIT_SOURCE_TOKEN == "testtoken"
-    assert config.GIT_SOURCE_USERNAME == "testusername"
+    assert config.GIT_SOURCE_TOKEN
+    assert config.GIT_SOURCE_USERNAME
     assert config.GIT_STATE_PATH == "/home/shaibu/Workspace/Python/data"
