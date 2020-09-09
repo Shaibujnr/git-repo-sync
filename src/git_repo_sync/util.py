b1576e71741af76fdf3b0c2fa89b948aea42e8b7
Shaibu Shaibu <s.shaibu.jnr@gmail.com>
2020-09-09 09:54:41 +0100

    provide util method to sync target repository

--- src/git_repo_sync/util.py
+++ src/git_repo_sync/util.py
@@ -1,6 +1,7 @@
 import os
-from git import Repo
+from git import Repo, Remote
 from git.cmd import Git
+from git.exc import GitCommandError
 from pathlib import Path
 from git_repo_sync.config import (
     GIT_STATE_PATH,
@@ -20,12 +21,16 @@ def does_repository_exist_locally(repository_name):
     return bool(os.path.exists(git_folder))
 
 
+def authenicated_url(username, token, git_url):
+    protocol, url = git_url.split("//")
+    return f"{protocol}//{username}:{token}@{url}"
+
+
 def clone_repository(git_url: str) -> Repo:
     repo_url = git_url
     if "@" not in repo_url:
         # no authentication details
-        protocol, url = git_url.split("//")
-        repo_url = f"{protocol}//{GIT_SOURCE_USERNAME}:{GIT_SOURCE_TOKEN}@{url}"
+        repo_url = authenicated_url(GIT_SOURCE_USERNAME, GIT_SOURCE_TOKEN, repo_url)
     repo_name = repo_url.split("/")[-1].split(".")[0]
     target_path = repo_folder_path / repo_name
     return Repo.clone_from(url=repo_url, to_path=target_path)
@@ -48,3 +53,24 @@ def update_repository(repo_name: str) -> Repo:
         git.pull(all=True)
 
     git.checkout(current_branch)
+
+
+def sync_target(repo_name, target_url, target_username, target_token):
+    repo_path = repo_folder_path / repo_name
+    repo = Repo(repo_path)
+    git = Git(working_dir=repo_path)
+    if "target" in repo.remotes:
+        Remote.remove(repo, "target")
+        # repo.remotes.target.rm()
+
+    authenticated_target_url = authenicated_url(
+        target_username, target_token, target_url
+    )
+    repo.create_remote("target", authenticated_target_url)
+    try:
+        git.push(["target", "*:*"])
+        git.push("target", all=True)
+    except GitCommandError as e:
+        checks = ["remote rejected", "deny updating a hidden ref"]
+        if any(check not in str(e) for check in checks):
+            raise e
--- tests/test_util.py
+++ tests/test_util.py
@@ -15,13 +15,32 @@ def test_folder_exists_but_not_as_repo():
     assert not util.does_repository_exist_locally("test_folder")
 
 
+def test_authenticated_url():
+    username = "testusername"
+    token = "testtoken"
+    url = "https://gitremote.com/username/repo.git"
+    expected_result = "https://testusername:testtoken@gitremote.com/username/repo.git"
+    result = util.authenicated_url(username, token, url)
+    assert expected_result == result
+
+
 def test_clone_repository():
     test_url = "https://bitbucket.org/shaibujnr/git_repos_sync.git"
     assert not util.does_repository_exist_locally("git_repos_sync")
-    repo = util.clone_repository(test_url)
+    util.clone_repository(test_url)
     assert util.does_repository_exist_locally("git_repos_sync")
 
 
 def test_update_repository():
     repo_name = "git_repos_sync"
     util.update_repository(repo_name)
+
+
+def test_sync_repository():
+    repo_name = "git_repos_sync"
+    util.sync_target(
+        repo_name,
+        "https://gitlab.com/shaibujnr/git_repos_sync.git",
+        "shaibujnr",
+        "Vwyrz_4az8FojXAWNH2x",
+    )
